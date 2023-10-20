44cac6a3b367553132a7ac45a36cd990305a7fce
IshitaSrivast <20cs3034@rgipt.ac.in>
2023-10-21 00:21:48 +0530

    nft

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+
+const Footer = () => {
+
+
+
+
+return (
+    <>
+      <div className="footer">
+      
+      
+       </div>
+    </>
+  );
+};
+
+export default Footer;
